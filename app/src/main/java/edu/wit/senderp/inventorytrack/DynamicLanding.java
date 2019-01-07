1de894cdde7d6c9f158a2751fb18e61f90d32f09
Paul-Sender <PaulSender99@gmail.com>
2019-01-06 20:14:24 -0500

    Initial commit

---
+++ app/src/main/java/edu/wit/senderp/inventorytrack/DynamicLanding.java
@@ -0,0 +1,13 @@
+package edu.wit.senderp.inventorytrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DynamicLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_landing);
+    }
+}
